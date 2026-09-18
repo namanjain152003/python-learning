b6480e8af3df5e660920819e9f952c6cc7cb1d52
NAMAN JAIN <namanjain152003@gmail.com>
2026-09-18 19:55:29 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,19 @@
+name="naman"
+age=23
+gender="male"
+
+isPrime=None
+isBool=True
+
+print(name)
+print("my name is :",name)
+print(type(gender))
+
+
+#this is a comment 
+''' 
+this is multi line comment
+
+'''
+
+#arithmetic operator(+,-,*,/,**,%)
