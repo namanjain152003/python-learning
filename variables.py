b5651ad327c27195c0e3be38031bdae3bdb9d327
NAMAN JAIN <namanjain152003@gmail.com>
2026-09-19 15:08:05 +0530

    type conversion

--- variables.py
+++ variables.py
@@ -20,3 +20,28 @@ this is multi line comment
 #relational operator(<,<=,>,>=,==,!=)
 #assignment operator(=,+=,-=,*=,/=)
 #logical operator(not , and , or)
+
+
+'''
+operator precedence
+()
+**
+*,/,%
++,-
+==,=<,>=,<,>
+not
+and 
+or
+
+'''
+
+#type conversion
+# two types of conversion
+
+# 1.implicit(doing on their own)
+# e.g  float+int=float
+
+# 2.explicit(done by developer)
+# e.g value=float(10)
+#     print(value, type(value))
+
