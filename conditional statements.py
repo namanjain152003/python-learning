bd537f8bf0d63e8336d68df5c66917b165af7d6d
NAMAN JAIN <namanjain152003@gmail.com>
2026-09-20 21:16:25 +0530

    conditional statements (if ,else, elif)

--- conditional statements.py
+++ conditional statements.py
@@ -1,5 +1,6 @@
 
 # if - else example
+# voting system
 age=21
 if age==21:
     print("you are eligible for vote")
@@ -7,6 +8,7 @@ else:
     print("you are not eligible")
     
 # elif example
+# traffic light system
 color="green"
 if color=="green":
     print("go")
