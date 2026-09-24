2416ff31f52c226aa1b7ed3777d75f808d5e5e22
NAMAN JAIN <namanjain152003@gmail.com>
2026-09-24 19:56:01 +0530

    functions and parameter

---
+++ functions.py
@@ -0,0 +1,19 @@
+# functions has two parts definition and calling part
+
+def hello():
+    print("hello world")
+    
+hello()
+
+# we can give parametes to the function
+
+def sum(a,b):
+    s=a+b
+    return s
+
+ans=sum(4,5)
+print(ans)
+
+#another method
+
+print(sum(4,5))
