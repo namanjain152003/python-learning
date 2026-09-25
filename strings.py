138cc674ba75f0364d65ac867a5013ec8875c2f0
NAMAN JAIN <namanjain152003@gmail.com>
2026-09-25 19:20:16 +0530

    slicing in strings

--- strings.py
+++ strings.py
@@ -10,3 +10,21 @@ print(word1+" "+word2)
 
 print(word1[2])   # this is the indexing in the strings
 print(word2[1])
+
+
+# slicing in the strings
+word3="naman"
+print(word3[2:4])       #[start index : end index] here end index value is  not included
+
+# the default value case
+# for start value we can give zero or blank
+# for end value we can give blank or len(word)
+
+print(word3[:])  # this will print the whole string
+print(word3[2:]) # this will print  from index 2 to last of the string
+
+
+# we can slice the string using the negative values as well
+# n -> -5  a -> -4  m-> -3  a-> -2  n -> -1
+
+print(word3[-4:-2])  # here also -2 is not included 
