caadb05eb5502cde78f8b2c904fe2eeec48fc83b
NAMAN JAIN <namanjain152003@gmail.com>
2026-09-25 15:41:10 +0530

    strings length , concatenation and indexing

---
+++ strings.py
@@ -0,0 +1,12 @@
+word1="i love"
+word2="python"
+
+print(len(word1))  # this is used to find the length of the strings
+print(len(word2))
+
+sentence=word1+" "+word2  # this is the concatenation of the two strings
+print(sentence)
+print(word1+" "+word2)
+
+print(word1[2])   # this is the indexing in the strings
+print(word2[1])
